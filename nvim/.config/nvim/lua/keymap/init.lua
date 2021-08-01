75e886b320ea197f2114fb00cdf47a7cfc16f0ce
Deep Dhanak <deep.dhanak@gmail.com>
2021-08-01 19:32:22 -0400

    Remove keymappings.lua

--- nvim/.config/nvim/lua/keymap/init.lua
+++ nvim/.config/nvim/lua/keymap/init.lua
@@ -27,28 +27,6 @@ local plug_map = {
     ["n|ga"] = map_cmd("v:lua.enhance_align('nga')"):with_expr(),
     ["x|ga"] = map_cmd("v:lua.enhance_align('xga')"):with_expr(),
 
-
-
-
- -- Regular stuff
--- utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
--- utils.map('i', 'jk', '<Esc>')           -- jk to escape
-
--- -- Telescope
--- utils.map('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
--- utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
--- utils.map('n', '<leader>fb', '<cmd>Telescope buffers<CR>')
--- utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
-
--- -- Vim Fugitive
--- utils.map('n', '<leader>gs', '<Esc>:Git<CR>')
-
--- -- Nvim Tree
--- utils.map('n', '<C-n>', ':NvimTreeToggle<CR>')
-
--- -- Easy Align
--- utils.map('n', 'ga', '<Plug>(EasyAlign)', {})
--- utils.map('x', 'ga', '<Plug>(EasyAlign)', {})   
 };
 
 bind.nvim_load_mapping(plug_map)
